c70fffa465efc42e3eaa71ac2a1a2b3a054ed711
Esteban Aguirre Martinez <estebanaguirre1997@gmail.com>
2022-12-07 12:23:32 -0500

    ajustes eliminar inicio dado que no funcionaba correctamente

--- adapter/postgresql.js
+++ adapter/postgresql.js
@@ -1,9 +1,9 @@
 const {connection} = require('../config/postgresql')
 
-validarBotActivo = (id) =>  new Promise((resolve,reject) => {
+validarBotActivo = () =>  new Promise((resolve,reject) => {
     try {
         connection.query(
-            `select ruta_archivo_pm2,nombre_archivo_ejecutar,nombre_aplicacion from tb_validacion_funcionamiento_bot WHERE ROUND(extract(epoch from (now()::timestamp - ultima_ejecucion::timestamp))/60) > 10 and pm2_activo = 1 and clinica_id= '${process.env.CLINICA_ID}'` , (error, results) => {
+            `select clinica_id,ruta_archivo_pm2,nombre_archivo_ejecutar,nombre_aplicacion from tb_validacion_funcionamiento_bot WHERE ROUND(extract(epoch from (now()::timestamp - ultima_ejecucion::timestamp))/60) > 10 and pm2_activo = 1` , (error, results) => {
             if(error) { 
                 console.log(error)
             }
@@ -14,4 +14,32 @@ validarBotActivo = (id) =>  new Promise((resolve,reject) => {
     }
 })
 
-module.exports = {validarBotActivo}
+ultimaValidacionBot = () =>  new Promise((resolve,reject) => {
+    try {
+        connection.query(
+            `UPDATE tb_validacion_funcionamiento_bot SET ultima_validacion = NOW() where pm2_activo = 1` , (error, results) => {
+            if(error) { 
+                console.log(error)
+            }
+            resolve(results.rows)
+        })
+    } catch (error) {
+        
+    }
+})
+
+ultimaEjecucionReinicio = (clinica_id) =>  new Promise((resolve,reject) => {
+    try {
+        connection.query(
+            `UPDATE tb_validacion_funcionamiento_bot SET ultimo_restart = NOW() WHERE clinica_id = '${clinica_id}' and pm2_activo = 1` , (error, results) => {
+            if(error) { 
+                console.log(error)
+            }
+            resolve(results.rows)
+        })
+    } catch (error) {
+        
+    }
+})
+
+module.exports = {validarBotActivo,ultimaValidacionBot,ultimaEjecucionReinicio}
